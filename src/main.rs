44efcf9fb8f7236fc1f08e9677afe20306ff2837
Darth Sharkie <darthsharkie+aoc@gmail.com>
2021-11-30 23:48:08 -0800

    Part 2 solved

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let lines: Vec<String> = load_file(&filename);
     
     part1(&lines);
+    part2(&lines);
 }
 
 fn load_file(filename: &String) -> Vec<String> {
@@ -32,4 +33,33 @@ fn part1(lines: &Vec<String>) {
 
     println!("Descended {} times!", descents);
     
+}
+
+fn part2(lines: &Vec<String>) {
+
+    // Rolling index into the vector (really, a ring buffer)
+    let mut index: usize = 0;
+    let mut depths = vec![0; 3];
+
+    // Depth tracking and descent counting
+    let mut previous_depth: i32 = i32::MAX;
+    let mut descents: i32 = 0;
+
+    for line in lines {
+        let depth: i32 = line.parse().expect("Invalid i32 value");
+        depths[index.rem_euclid(3)] = depth;
+
+        // If three values exist, then look for deeper water
+        if index >= 2 {
+            let depth_sum = depths[0] + depths[1] + depths[2];
+            if depth_sum > previous_depth {
+                descents += 1;
+                println!("Descending!");
+            }
+            previous_depth = depth_sum;
+        }
+        index += 1;
+    }
+
+    println!("Descended {} times!", descents);
 }
