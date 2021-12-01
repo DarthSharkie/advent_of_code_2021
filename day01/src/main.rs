c17a2a6c940c6973c0919c0619488dac038a2eb2
Darth Sharkie <darthsharkie+aoc@gmail.com>
2021-12-01 00:53:46 -0800

    Listen to clippy
    clippy tells me how to write more ferrous code.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -8,17 +8,17 @@ fn main() {
     part2(&lines);
 }
 
-fn load_file(filename: &String) -> Vec<u32> {
+fn load_file(filename: &str) -> Vec<u32> {
     let contents = fs::read_to_string(filename).expect("Error reading file!");
     let lines: Vec<u32> = contents.trim().lines().map(|s| s.parse().expect("Invalid u32 value")).collect();
     lines
 }
 
-fn part1(lines: &Vec<u32>) {
+fn part1(depths: &[u32]) {
     let mut previous_depth: u32 = u32::MAX;
     let mut descents: u32 = 0;
 
-    for depth in lines {
+    for depth in depths {
         if *depth > previous_depth {
             descents += 1;
         }
@@ -28,24 +28,16 @@ fn part1(lines: &Vec<u32>) {
     println!("Descended {} times!", descents);
 }
 
-fn part2(lines: &Vec<u32>) {
-
-    // Rolling index into the vector (really, a ring buffer)
-    let mut index: usize = 0;
-    let mut depths = vec![0; 3];
-
-    // Depth tracking and descent counting
+fn part2(depths: &[u32]) {
     let mut descents: u32 = 0;
 
-    for depth in lines {
+    for (index, depth) in depths.into_iter().enumerate() {
         // If three values exist, then look for deeper water
         if index >= 3 {
-            if *depth > depths[index.rem_euclid(3)] {
+            if *depth > depths[index - 3] {
                 descents += 1;
             }
-            depths[index.rem_euclid(3)] = *depth;
         }
-        index += 1;
     }
 
     println!("Descended {} times!", descents);
