19d3dcaf5f8943baf55b8c182b67afc75da17f07
Darth Sharkie <darthsharkie+aoc@gmail.com>
2021-12-02 22:23:22 -0800

    Part 1 solution

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::str::FromStr;
 
 // Replace with day-specific struct/enums
+/*
 struct Move {
     direction: String,
     distance: i32,
@@ -15,19 +16,41 @@ impl FromStr for Move {
 
     }
 }
+*/
 
 fn main() {
     let filename = String::from("input03.txt");
     let contents = fs::read_to_string(filename).expect("Error reading file!");
 
-    let moves: Vec<Move> = contents.trim().lines().map(|s| s.parse().unwrap()).collect();
+    let readings: Vec<String> = contents.trim().lines().map(str::trim).map(String::from).collect();
 
-    part1(&moves);
-    part2(&moves);
+    part1(&readings);
+    part2(&readings);
 }
 
-fn part1(moves: &[Move]) {
+fn part1(readings: &[String]) {
+    let mut gamma: [usize; 12] = [0; 12];
+
+    for reading in readings {
+        let mut i: usize = 0;
+        for bit in reading.bytes() {
+            if bit == b'1' {
+                gamma[i] += 1;
+            }
+            i += 1;
+        }
+    }
+    gamma = gamma.map(|count| count >= readings.len() / 2).map(|b| b as usize);
+    let gamma_value: u32 = bits_to_u32(&gamma);
+    let epsilon_value: u32 = 2u32.pow(12) - gamma_value - 1;
+    let power = gamma_value * epsilon_value;
+    println!("Power: {}", power);
 }
 
-fn part2(moves: &[Move]) {
+fn part2(readings: &[String]) {
 }
+
+fn bits_to_u32(bits: &[usize]) -> u32 {
+    bits.iter().fold(0, |acc, b| acc * 2 + b) as u32
+}
+
