adaaf77d0d3c26fb7fd69aabc82754a08f1f5966
Darth Sharkie <darthsharkie+aoc@gmail.com>
2021-12-04 01:55:54 -0800

    Part 2 solution, finally
    Those darn zero values messing up my supposedly elegant solve...

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -3,30 +3,47 @@ use std::fs;
 // Replace with day-specific struct/enums
 struct Card {
     slots: [[u32; 5]; 5],
+    marks: [[bool; 5]; 5],
     score: u32,
+	draws: u32,
 }
 
 impl Card {
-    fn from_chunk(chunk: Vec<&str>) -> Card {
+    fn from_chunk(chunk: &str) -> Card {
         let mut board: [[u32; 5]; 5] = [[0; 5]; 5];
-        for (i, line) in chunk.iter().enumerate() {
+        for (i, line) in chunk.lines().enumerate() {
             let row: Vec<u32> = line.split_whitespace().map(|num| num.parse().unwrap()).collect();
             for (j, num) in row.iter().enumerate() {
                 board[i][j] = *num;
             }
         }
-        Card { slots: board, score: 0 }
+        Card { slots: board, marks: [[false; 5]; 5], score: 0, draws: 0 }
     }
 
+	fn play(&mut self, moves: &[u32]) {
+		for mv in moves {
+			self.mark(mv);
+			if self.score > 0 {
+                break;
+			}
+		}
+	}
+
+
     fn mark(&mut self, value: &u32) {
+		if self.score > 0 {
+			return
+		}
+		self.draws += 1;
         for i in 0..5 {
             for j in 0..5 {
                 if self.slots[i][j] == *value {
-                    self.slots[i][j] = 0;
+                    self.marks[i][j] = true;
+                    self.score = value * self.evaluate();
+                    return
                 }
             }
         }
-        self.score = self.evaluate();
     }
 
     fn score(&self) -> u32 {
@@ -34,17 +51,19 @@ impl Card {
     }
 
     fn evaluate(&self) -> u32 {
-        let mut row_sums: [u32; 5] = [0; 5];
-        let mut col_sums: [u32; 5] = [0; 5];
-        for i in 0..5 {
-            for j in 0..5 {
-                row_sums[i] += self.slots[i][j];
-                col_sums[j] += self.slots[i][j];
+        let mut row_wins: [bool; 5] = [true; 5];
+        let mut col_wins: [bool; 5] = [true; 5];
+        for (i, row_win) in row_wins.iter_mut().enumerate() {
+            for (j, col_win) in col_wins.iter_mut().enumerate() {
+                *row_win &= self.marks[i][j];
+                *col_win &= self.marks[i][j];
             }
         }
 
-        if row_sums.iter().min().unwrap() == &0u32 || col_sums.iter().min().unwrap() == &0u32 {
-            row_sums.iter().sum::<u32>()
+        if row_wins.iter().any(|&row| row) || col_wins.iter().any(|&col| col) {
+			let values = self.slots.iter().flat_map(|row| row.iter());
+			let marked = self.marks.iter().flat_map(|row| row.iter());
+			values.zip(marked).map(|(&value, &mark)| if mark {0u32} else {value}).sum()
         } else {
             0
         }
@@ -56,46 +75,26 @@ fn main() {
     let filename = String::from("input04.txt");
     let contents = fs::read_to_string(filename).expect("Error reading file!");
     
-    let lines: Vec<&str> = contents.trim().lines().map(str::trim).collect();
-    let moves: Vec<u32> = lines[0].split(',').map(|num| num.parse().unwrap()).collect();
+    let (moves, boards) = contents.split_once("\n\n").unwrap();
+	let moves: Vec<u32> = moves.split(',').map(|num| num.parse().unwrap()).collect();
 
-    part1(&lines, &moves);
-    part2(&lines, &moves);
+    println!("Part 1: {}", part1(&boards, &moves));
+    println!("Part 2: {}", part2(&boards, &moves));
+
 }
 
-fn part1(lines: &Vec<&str>, moves: &[u32]) {
-    let mut cards: Vec<Card> = Vec::new();
-    for chunk in lines[2..].chunks(6) {
-        cards.push(Card::from_chunk(chunk[0..5].to_vec()));
-    }
+fn part1(boards: &str, moves: &[u32]) -> u32 {
+    let mut cards: Vec<Card> = boards.split("\n\n").map(Card::from_chunk).collect();
 
-    'outer: for mv in moves {
-        for card in &mut cards {
-            card.mark(&mv);
-            let sum = card.score();
-            if sum > 0 {
-                println!("Value: {}, Sum: {}", mv, sum);
-                break 'outer;
-            }
-        }
-    }
+    cards.iter_mut().for_each(|card| card.play(moves));
+    cards.sort_by(|lhs, rhs| lhs.draws.cmp(&rhs.draws));
+    cards[0].score()
 }
 
-fn part2(lines: &Vec<&str>, moves: &[u32]) {
-    let mut cards: Vec<Card> = Vec::new();
-    for chunk in lines[2..].chunks(6) {
-        cards.push(Card::from_chunk(chunk[0..5].to_vec()));
-    }
+fn part2(boards: &str, moves: &[u32]) -> u32 {
+    let mut cards: Vec<Card> = boards.split("\n\n").map(Card::from_chunk).collect();
 
-    for mv in moves {
-        for (i, card) in cards.iter_mut().enumerate() {
-            card.mark(&mv);
-            let score = card.score();
-            if score > 0 {
-                println!("Card eliminated: {:?}, Score: {}", card.slots, mv * score);
-            }
-        }
-        cards.retain(|card| card.score() == 0);
-    }
+    cards.iter_mut().for_each(|card| card.play(moves));
+    cards.sort_by(|lhs, rhs| lhs.draws.cmp(&rhs.draws));
+    cards[cards.len() - 1].score()
 }
-
