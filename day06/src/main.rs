6c8547a42af2bcd8dc0ebbe2ac04bf0355ea7e95
Darth Sharkie <darthsharkie+aoc@gmail.com>
2021-12-05 23:26:01 -0800

    Restructure a bit to test, etc.
    Aiming toward a simpler template with obvious edits in the future.
    Also adding tests for the sample bits.

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,19 +1,29 @@
 use std::fs;
 
 fn main() {
-    let filename = String::from("input06.txt");
-    let contents = fs::read_to_string(filename).expect("Error reading file!");
+    println!("Part 1: {}", part1(parse("input06.txt"))); // 386536
+    println!("Part 2: {}", part2(parse("input06.txt"))); // 1732821262171
+}
 
+fn parse(filename: &str) -> [usize; 9] {
+    let contents = fs::read_to_string(filename).expect("Error reading file!");
     let mut school: [usize; 9] = [0; 9];
     for timer in contents.trim().split(',').map(str::parse::<usize>).map(Result::unwrap) {
         school[timer] += 1;
     }
-
-    println!("Part 1: {}", tick(&mut school.clone(), 80));  // 386536
-    println!("Part 2: {}", tick(&mut school.clone(), 256)); // 1732821262171
+    school
 }
 
-fn tick(school: &mut [usize; 9], days: usize) -> usize {
+fn part1(school: [usize; 9]) -> usize {
+    tick(school, 80)
+}
+
+fn part2(school: [usize; 9]) -> usize {
+    tick(school, 256)
+}
+
+fn tick(school: [usize; 9], days: usize) -> usize {
+    let mut school = school.clone();
     for _ in 0..days {
         let new_fish = school[0];
         for timer in 0..8 {
@@ -25,3 +35,12 @@ fn tick(school: &mut [usize; 9], days: usize) -> usize {
     school.iter().sum()
 }
 
+#[test]
+fn test_part1() {
+    assert_eq!(part1(parse("sample.txt")), 5934);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(parse("sample.txt")), 26984457539);
+}
