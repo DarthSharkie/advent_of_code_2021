6450dececfe07db8407312f18a6068ce30eb60b4
Darth Sharkie <darthsharkie+aoc@gmail.com>
2021-12-06 22:40:53 -0800

    Day 7 part 1 solution

---
+++ day07/src/main.rs
@@ -0,0 +1,43 @@
+use std::fs;
+
+fn main() {
+    println!("Part 1: {}", part1(parse("input07.txt"))); // 386536
+    println!("Part 2: {}", part2(parse("input07.txt"))); // 1732821262171
+}
+
+fn parse(filename: &str) -> Vec<i32> {
+    let contents = fs::read_to_string(filename).expect("Error reading file!");
+    contents.trim().split(',').map(str::parse).map(Result::unwrap).collect()
+}
+
+fn part1(positions: Vec<i32>) -> i32 {
+    let min = positions.iter().min().unwrap();
+    let max = positions.iter().max().unwrap();
+    let mut fuel = i32::MAX;
+    let mut best_position = 0;
+
+    for pos in *min..=*max {
+        let sum = positions.iter().fold(0, |acc, x| acc + i32::abs(pos - x));
+        if sum < fuel {
+            fuel = sum;
+            best_position = pos;
+        }
+    }
+    println!("Position {} is best", best_position);
+    fuel
+}
+
+fn part2(positions: Vec<i32>) -> i32 {
+    0
+}
+
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1(parse("sample.txt")), 37);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(parse("sample.txt")), 0);
+}
