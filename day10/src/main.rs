1fc4f30986c9596042467c9bf4ea0c44e08d5e7f
Darth Sharkie <darthsharkie+aoc@gmail.com>
2021-12-09 21:30:55 -0800

    Part 1 solved

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,25 +1,77 @@
 use std::fs;
 use std::str::FromStr;
 use std::collections::VecDeque;
-use std::collections::HashSet;
+use std::collections::HashMap;
+
+type Nav = Vec<Vec<char>>;
 
 fn main() {
     println!("Part 1: {}", part1(&parse("input10.txt"))); // 518
     println!("Part 2: {}", part2(&parse("input10.txt"))); // 949905
 }
 
-fn parse(filename: &str) -> Floor {
+fn parse(filename: &str) -> Nav {
     let contents = fs::read_to_string(filename).expect("Error reading file!");
     contents.trim().lines().map(|s| {
-
+		s.chars().collect()
     }).collect()
 }
 
-fn part1() -> i32 {
-    0
+fn part1(nav: &Nav) -> i32 {
+    let mut counts: HashMap<char, i32> = HashMap::new();
+	for cmd in nav {
+        let mut stack = VecDeque::new();
+        for ins in cmd {
+            let expected = match ins {
+                '[' | '(' | '{' | '<' => { stack.push_front(ins); None },
+                '>' => if let Some('<') = stack.get(0) {
+                    stack.pop_front();
+                    None
+                } else {
+                    Some(ins)
+                },
+                '}' => if let Some('{') = stack.get(0) {
+                    stack.pop_front();
+                    None
+                } else {
+                    Some(ins)
+                },
+                ')' => if let Some('(') = stack.get(0) {
+                    stack.pop_front();
+                    None
+                } else {
+                    Some(ins)
+                },
+                ']' => if let Some('[') = stack.get(0) {
+                    stack.pop_front();
+                    None
+                } else {
+                    Some(ins)
+                },
+                _ => panic!("Shouldn't happen"),
+            };
+            match expected {
+                Some(close) => {
+                    println!("Mismatch: {}", close);
+                    let entry = counts.entry(*close).or_insert(0);
+                    *entry += 1;
+                    break;
+                },
+                _ => (),
+            }
+        }
+        if stack.len() > 0 {
+            println!("Incomplete!");
+        }
+	}
+    println!("Counts: {:?}", counts);
+    (*counts.entry(')').or_default() * 3) + 
+    (*counts.entry(']').or_default() * 57) + 
+    (*counts.entry('}').or_default() * 1197) + 
+    (*counts.entry('>').or_default() * 25137) 
 }
 
-fn part2() -> i32 {
+fn part2(nav: &Nav) -> i32 {
     0
 }
 
@@ -27,7 +79,7 @@ fn part2() -> i32 {
 
 #[test]
 fn test_part1() {
-    assert_eq!(part1(&parse("sample.txt")), 0);
+    assert_eq!(part1(&parse("sample.txt")), 26397);
 }
 
 #[test]
