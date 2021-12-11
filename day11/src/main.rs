14c802f0f7bd679479c9c1989c7d18cc03cae3b8
Darth Sharkie <darthsharkie+aoc@gmail.com>
2021-12-10 22:17:41 -0800

    Add sample and my input

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use std::collections::VecDeque;
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Part 1: {}", part1(&parse("input11.txt"))); // 290691
+    println!("Part 2: {}", part2(&parse("input11.txt"))); // 2768166558
+}
+
+fn parse(filename: &str) -> Nav {
+    let contents = fs::read_to_string(filename).expect("Error reading file!");
+    contents.trim().lines().map(|s| {
+		s.chars().collect()
+    }).collect()
+}
+
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1(&parse("sample.txt")), 1656);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(&parse("sample.txt")), 288957);
 }
